707e4749082443a6b5167fed4fae059d8f8e79b5
samsularifin05 <samsul026@gmail.com>
2022-02-25 10:56:32 +0700

    fixing dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\ModelBarang;
 class DashboardController extends Controller
 {
     
     public function index()
     {
-        return view('dashboard.index');
+        $databarang = ModelBarang::all();
+        $data = array(
+            'databarang' => count($databarang),
+        );
+        return view('dashboard.index',compact('data'));
     }
 }

a6cd16ecd797b4a987654c71889ea67971fe0890
samsularifin05 <samsul026@gmail.com>
2022-02-27 20:13:39 +0700

    last update

--- resources/js/module/helper.js
+++ resources/js/module/helper.js
@@ -38,8 +38,6 @@ window.formatRupiah = function(angka){
     sisa     		= split[0].length % 3,
     rupiah     		= split[0].substr(0, sisa),
     ribuan     		= split[0].substr(sisa).match(/\d{3}/gi);
-
-    // tambahkan titik jika yang di input sudah menjadi angka ribuan
     if(ribuan){
         separator = sisa ? '.' : '';
         rupiah += separator + ribuan.join('.');
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js/app.js').version()
-    .sass('resources/css/app.scss', 'public/css/app.css', []);
+    .sass('resources/css/app.scss', 'public/css/app.css').minify('public/css/app.css');
 
     mix.setResourceRoot('../');
 mix.browserSync('127.0.0.1:8000');
